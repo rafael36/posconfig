c533be8817bea00c70e60e8baf6ef40fc2b5ae57
rafael36 <66216646+rafael36@users.noreply.github.com>
2025-03-28 20:04:05 -0300

    Update instalaçao.sh

--- instalaçao.sh
+++ instalaçao.sh
@@ -16,6 +16,23 @@ mkdir -p /home/rafael/.config/alacritty
 mkdir -p /home/rafael/.config/hypr
 mkdir -p /home/rafael/.local/share/applications
 mkdir -p /usr/share/nemo/actions
+mkdir /mnt/hd2
+
+
+# montar hd2 permanente
+echo '
+# /dev/sda1
+UUID=66611ca8-7791-4a23-93d3-dcb7daf5c577  /mnt/hd2  ext4  defaults,noatime  0  2' | sudo tee -a /etc/fstab
+
+# python
+python -m venv /home/rafael/venv
+
+echo 'source /home/rafael/venv/bin/activate' >> /home/rafael/.bashrc
+
+sed -i '1s|^|VIRTUAL_ENV_DISABLE_PROMPT=1\n|' /home/rafael/venv/bin/activate
+
+/home/rafael/venv/bin/pip install yt-dlp selenium bs4
+
 
 # Mover wallpapers para a pasta home
 mv wallhaven-85dpxo_1920x1080.png wallhaven-7331ko_1920x1080.png /home/rafael/
