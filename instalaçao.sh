2422fa5a054a1fa35aff3108e1f72d0e6056e99d
rafael <oliverarafael911@gmail.com>
2025-03-30 15:18:13 -0300

    atualizaçao dia 30, 4

--- instalaçao.sh
+++ instalaçao.sh
@@ -14,10 +14,23 @@ cd configpasta
 
     # Criar diretórios necessários
     mkdir -p /home/rafael/.themes
+    mkdir -p /home/rafael/.config/gtk-3.0
+    mkdir -p /home/rafael/.config/ulauncher
+    mkdir -p /home/rafael/.local/share/nwg-look
+    mkdir -p /home/rafael/.config/xsettingsd
+    mkdir -p /home/rafael/.themes/MyBreeze-Dark-GTK/gtk-4.0
     mkdir -p /home/rafael/.config/waybar
     mkdir -p /home/rafael/.config/alacritty
     mkdir -p /home/rafael/.config/hypr
     mkdir -p /home/rafael/.local/share/applications
+
+    mv nwg-look/gsettings /home/rafael/.local/share/nwg-look
+    mv gtk-3.0/settings.ini /home/rafael/.config/gtk-3.0
+    mv .gtkrc-2.0 /home/rafael
+    mv settings.json /home/rafael/.config/ulauncher
+    mv xsettingsd/xsettingsd.conf /home/rafael/.config/xsettingsd
+
+
 
 
     # Extrair e mover tema GTK
@@ -45,19 +58,19 @@ UUID=66611ca8-7791-4a23-93d3-dcb7daf5c577  /mnt/hd2  ext4  defaults,noatime  0  
 
     # Configuração do Docker
     mkdir /etc/docker
-    touch /etc/docker/daemon.json
-    echo "{
-  "data-root": "/mnt/hd2/docker"
-}" > /etc/docker/daemon.json
+    mv daemon.json /etc/docker
     chmod 777 /etc/docker
     chmod 777 /mnt/hd2/docker
     systemctl enable docker
+    systemctl start docker
 
     # Configuração do Android AVD
     echo "ANDROID_SDK_HOME=/mnt/hd2/android-avd
 ANDROID_AVD_HOME=/mnt/hd2/android-avd/.android/avd
 " >> /etc/environment
 
+
+pacman -S steam
 
 
 mv open_in_code.nemo_action open_in_terminal.nemo_action /usr/share/nemo/actions
